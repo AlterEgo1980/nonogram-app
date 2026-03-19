449d4ea54a93fdb3967ec663310f7caed305bf6d
Nir <gedalianir@gmail.com>
2026-03-20 01:49:08 +0200

    change0

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
 const CACHE_NAME = 'nonogram-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/engine.js',
-  '/levels.js',
-  '/sprite1.jpg',
-  '/logo1.png'
+  './',
+  './index.html',
+  './engine.js',
+  './levels.js',
+  './sprite1.jpg',
+  './logo1.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
